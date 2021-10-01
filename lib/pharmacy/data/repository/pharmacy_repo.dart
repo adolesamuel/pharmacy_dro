e3f52260f18feeabb8e48c5c36092b362cb96f3a
adolesamuel <adolesamuel4@gmail.com>
2021-10-01 02:40:02 +0100

    finished data layer

--- lib/pharmacy/data/repository/pharmacy_repo.dart
+++ lib/pharmacy/data/repository/pharmacy_repo.dart
@@ -24,13 +24,53 @@ class DrugRepository {
     }
   }
 
-
-  Future<Either<Failure,bool>> addDrugtoCart() async{
-    try{
+  Future<Either<Failure, bool>> addDrugtoCart(Drug newDrug) async {
+    try {
       //add to cart and return true
-      return Right(await drugLocalDataSource.addDrugtoCart())
-    }on Exception catch(e){
-      return Left(Failure())
+      return Right(await drugLocalDataSource
+          .addDrugToCart(newDrug)
+          .timeout(Duration(seconds: 10), onTimeout: () {
+        throw Exception('Time out Error ');
+      }));
+    } on Exception catch (e) {
+      return Left(Failure('Error from Add to Cart', e.toString()));
+    }
+  }
+
+  Future<Either<Failure, bool>> removeDrugFromCart(Drug presentDrug) async {
+    try {
+      return Right(await drugLocalDataSource
+          .removeDrugFromCart(presentDrug)
+          .timeout(Duration(seconds: 10), onTimeout: () {
+        throw Exception('Time out Error ');
+      }));
+    } on Exception catch (e) {
+      return Left(Failure('Error from remove from Cart', e.toString()));
+    }
+  }
+
+  Future<Either<Failure, bool>> removeAllDrugsFromCart() async {
+    try {
+      return Right(await drugLocalDataSource
+          .removeAllDrugsFromCart()
+          .timeout(Duration(seconds: 10), onTimeout: () {
+        throw Exception('Time out Error ');
+      }));
+    } on Exception catch (e) {
+      return Left(
+          Failure('Error from remove all drugs from Cart', e.toString()));
+    }
+  }
+
+  Future<Either<Failure, List<Drug>>> getDrugsFromCart() async {
+    try {
+      return Right(await drugLocalDataSource
+          .getDrugsInCart()
+          .timeout(Duration(seconds: 10), onTimeout: () {
+        throw Exception('Time out Error ');
+      }));
+    } on Exception catch (e) {
+      return Left(Failure('Error from Get all drugs from cart', e.toString()));
     }
   }
 }
--- lib/pharmacy/data/sources/local_source.dart
+++ lib/pharmacy/data/sources/local_source.dart
@@ -1,8 +1,96 @@
 //this queries the local data source Hive
 import 'package:hive/hive.dart';
+import 'package:pharmacy_dro/pharmacy/data/models/drug_model.dart';
 
 class DrugLocalDataSource {
-  Future<bool> addDrugToCart(Drug drug) async {
-    final box = await Hive.openBox('Cart');
+  Future<bool> addDrugToCart(Drug newDrug) async {
+    final cartbox = await Hive.openBox('cart');
+    List<Drug> drugs = [];
+    try {
+      List<String> drugsAsStringList = [];
+      final currentStrings = cartbox.get('cart');
+
+      if (currentStrings != null) {
+        for (final String string in currentStrings) {
+          final drug = Drug.fromJson(string);
+          drugs.add(drug);
+        }
+        drugs.add(newDrug);
+        for (final drug in drugs) {
+          final drugString = drug.toJson();
+          drugsAsStringList.add(drugString);
+        }
+      } else {
+        drugsAsStringList.add(newDrug.toJson());
+      }
+      await cartbox.put('cart', drugsAsStringList);
+      return true;
+    } catch (e) {
+      throw Exception('Add Drug to Cart Error');
+    } finally {
+      cartbox.close();
+    }
+  }
+
+  Future<bool> removeDrugFromCart(Drug presentDrug) async {
+    final cartbox = await Hive.openBox('cart');
+    List<Drug> drugs = [];
+    try {
+      List<String> drugsAsStringList = [];
+      final currentStrings = cartbox.get('cart');
+
+      if (currentStrings != null) {
+        for (final String string in currentStrings) {
+          final drug = Drug.fromJson(string);
+          drugs.add(drug);
+        }
+        drugs.remove(presentDrug); //actual remove function
+        for (final drug in drugs) {
+          final drugString = drug.toJson();
+          drugsAsStringList.add(drugString);
+        }
+      }
+      await cartbox.put('cart', drugsAsStringList);
+      return true;
+    } catch (e) {
+      throw Exception('Remove Drug From Cart Error');
+    } finally {
+      cartbox.close();
+    }
+  }
+
+  Future<List<Drug>> getDrugsInCart() async {
+    final box = await Hive.openBox('cart');
+    List<Drug> drugs = [];
+
+    try {
+      final drugsAsStringList = box.get('cart');
+      if (drugsAsStringList != null) {
+        for (final String drugString in drugsAsStringList) {
+          final drug = Drug.fromJson(drugString);
+          drugs.add(drug);
+        }
+        return drugs;
+      } else {
+        return drugs;
+      }
+    } catch (e) {
+      throw Exception('Get Drugs Error');
+    } finally {
+      box.close();
+    }
+  }
+
+  Future<bool> removeAllDrugsFromCart() async {
+    final box = await Hive.openBox('cart');
+    List<String> empty = [];
+    try {
+      await box.put('cart', empty);
+      return true;
+    } catch (e) {
+      throw Exception('Remove all drugs Error');
+    } finally {
+      box.close();
+    }
   }
 }
